8f2d9fa76cf31387e4feb8e18a580927f443ae2b
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-30 02:31:34 -0500

    did basically nothing but at least the plane moves

--- Final Project - Flight Simulator/script.js
+++ Final Project - Flight Simulator/script.js
@@ -1,10 +1,59 @@
 //global variables
-var cnv = document.getElementById("myCanvas"); //gets canvas
-var ctx = cnv.getContext("2d"); //sets context to 2d
+var cnv;
+var ctx;
+var plane;
+var arrFlights = new Array();
+var nbSteps = 100;
+var backgroudImage = new Image();
+backgroudImage.src = "img/Canada-1280-1107.png";
 
-var backgroudImage = new Image(1280, 1107);
-backgroundImage.src = "Canada-1280-1107.png";
+function Plane(xCoordinate, yCoordinate, imageSrc = "img/plane.jpg") {
+    this.step = 0;
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    this.imageSrc = imageSrc;
+    this.imageWidth = 40;
+    this.imageHeight = 40;
+    this.destX = 500;
+    this.destY = 500;
+    this.xMove = (this.destX - this.xCoordinate)/100;
+    this.yMove = (this.destY - this.yCoordinate)/100;
+    this.draw = function() {
+        var planeImage = new Image();
+        planeImage.src = this.imageSrc;
+        ctx.drawImage(planeImage, this.xCoordinate, this.yCoordinate, this.imageWidth, this.imageHeight);
+    }
+    this.move = function() {
+        if (this.step < nbSteps) {
+            this.step++;
+            this.xCoordinate += this.xMove;
+            this.yCoordinate += this.yMove;
+            this.draw();
+        }
+    }
+}
 
 $(document).ready(function() {
-    ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
+    cnv = document.getElementById("mapCanvas");
+    ctx = cnv.getContext("2d");
+    ctx.drawImage(backgroudImage, 0, 0, cnv.width, cnv.height);
+
+    $("#mapCanvas").click(function() {
+
+        plane = new Plane(Math.random() * 1000, Math.random() * 533);
+        //plane.draw();
+        setInterval(function(){ 
+            ctx.clearRect(0,0, cnv.width, cnv.height);
+            ctx.drawImage(backgroudImage, 0, 0, cnv.width, cnv.height);
+            plane.move();
+            
+        }, 75);
+        
+    });
+
+    $.get("capitals.json", function(data) {
+        for (var i = 0; i < data.flights.length; i++){
+            console.log(data.flights[i]);
+        }
+    });
 });
